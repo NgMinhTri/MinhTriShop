00570855aa9295d8da82412f26bcb0155090c57b
NgMinhTri <38482726+NgMinhTri@users.noreply.github.com>
2020-11-25 21:47:27 +0700

    Thiết kế class cho phần Entity Framework Code First

---
+++ MinhTriShop.Model/Models/Footer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhTriShop.Model.Models
+{
+    [Table("Footers")]//chiỉ ra cái class này sẽ gen ra tên bảng trong csdl ntn
+    public class Footer
+    {
+        [Key]
+        [MaxLength(50)]
+        public string ID { get; set; }
+        [Required]
+        public string Content { get; set; }
+    }
+}
